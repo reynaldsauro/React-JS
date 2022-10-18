c406a8eb480118da5a0be1a7143c52f46c672cbf
unknown <naldsauro@gmail.com>
2022-10-18 11:26:27 +0800

    first commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Dashboard = () => {
+    return ( 
+    
+        <div className="dashboard">
+            <img src="/images/1.png"/>
+            <h1>Welcome to Mater Dei College</h1>
+        </div>
+     );
+}
+ 
+export default Dashboard;
